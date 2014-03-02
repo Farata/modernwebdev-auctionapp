0e49f512569cff6715acb583799eea8535d6f77f
Anton Moiseev <a@antonmoiseev.com>
2014-03-02 04:45:01 +0400

    Untouched version generated by Yeoman AngularJS generator.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('v2App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
